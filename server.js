38da19ca233ae9ac39c5a7e977c49ab0d1956eec
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-03-11 16:57:53 -0400

    user routes added/ no middleware applied

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const postRouter = require("./posts/postRouter.js");
+const userRouter = require("./users/userRouter.js");
 
 const server = express();
+
+server.use(express.json());
+
+// server.use("/api/posts", postRouter);
+
+server.use("/api/users", userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,99 @@
 const express = require('express');
-
+const Users = require("./userDb.js");
+const Posts = require("../posts/postDb.js");
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  // do your magic!
+  
+  Users.insert(req.body)
+		.then(added => {
+			res.status(201).json(added);
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(500).json({ error: "error adding user" });
+		});
 });
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+  
+ Users.getUserPosts(req.params.id)
+		.then(userPosts => {
+      Posts.insert(req.body)
+				.then(post => {
+					res.status(201).json(post);
+				})
+				.catch(error => {
+					console.log(error);
+					res.status(500).json({ error: "error posting" });
+				});
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(500).json({ error: "error fetching user posts" });
+		});
+  
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+ 
+  Users.get(req.query)
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "error retrieving users" });
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  
+  Users.getById(req.params.id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).json({ error: "error fetching user by id" });
+    });
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+     
+			res.status(200).json(posts);
+		})
+		.catch(error => {
+			console.log(error);
+			res.status(500).json({ error: "error fetching user posts" });
+		});
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  
+  Users.remove(req.params.id)
+    .then(deleted => {
+      res.status(200).json({ message: `user ${req.params.id} has been nuked`});
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "error deleting user" });
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  
+  Users.update(req.params.id, req.body)
+    .then(updated => {
+      res.status(201).json({message: "username successfully updated"});
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ error: "error updating username" });
+    });
 });
 
 //custom middleware
